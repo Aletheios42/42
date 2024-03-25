723fdb0e968b2918fa1ee450d449d52e91c89076
AlejandroPintosAlcarazo <alejandropintosdev@gmail.com>
2024-03-25 08:31:50 +0100

    oh shit here we go again

---
+++ 06_Fdf/Src/init.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cado-car <cado-car@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 10:36:24 by cado-car          #+#    #+#             */
+/*   Updated: 2021/10/05 22:34:23 by cado-car         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fdf.h"
+
+void init_fdf(t_fdf **fdf)
+{
+	*fdf = (t_fdf *)malloc(sizeof(t_fdf));
+	if (!*fdf)
+		return ;
+	(*fdf)->mlx = mlx_init();
+	(*fdf)->win = mlx_new_window((*fdf)->mlx, (*fdf)->win_x, (*fdf)->win_y, WINDOW_NAME);
+  (*fdf)->image = mlx_new_image(mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	(*fdf)->buffer = mlx_get_data_addr((*fdf)->image, &((*fdf)->pixel_bits), \
+			&((*fdf)->line_bytes), &((*fdf)->endian);
+}
+
+t_map	*init_map(void)
+{
+	t_map	*map;:warn("%s");
+
+	map = malloc(sizeof(t_map));
+	if (!map)
+		return (NULL);
+	map->coordinates = NULL;
+	map->max_x = 0;
+	map->max_y = 0;
+	map->max_z = 0;
+	map->min_z = 0;
+	return (map);
+}
+
+t_image	*init_image(void *mlx)
+{
+	t_image	*image;
+
+	image = malloc(sizeof(t_image));
+	if (!image)
+		return (NULL);
+	}
+
+t_cam	*init_cam(t_map *map)
+{
+	t_cam	*cam;
+
+	cam = malloc(sizeof(t_cam));
+	if (!cam)
+		return (NULL);
+	cam->projection = ISOMETRIC;
+	cam->color_pallet = FALSE;
+	cam->scale_factor = scale_to_fit(map);
+	cam->scale_z = 1;
+	cam->move_x = WINDOW_WIDTH / 2;
+	cam->move_y = WINDOW_HEIGHT / 2;
+	cam->alpha = 0;
+	cam->beta = 0;
+	cam->gamma = 0;
+	return (cam);
+}
+
+t_line	*init_line(t_point start, t_point end, t_fdf *fdf)
+{
+	t_line	*line;
+
+	line = malloc(sizeof(t_line));
+	if (!line)
+		return (NULL);
+	line->start.x = start.x;
+	line->start.y = start.y;
+	line->start.z = start.z;
+	line->start.color = start.color;
+	line->end.x = end.x;
+	line->end.y = end.y;
+	line->end.z = end.z;
+	line->end.color = end.color;
+	line->transform_z = max((fdf->map->max_z - fdf->map->min_z), \
+		max(fdf->map->max_x, fdf->map->max_y));
+	return (line);
+}
---
+++ 06_Fdf/Src/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cado-car <cado-car@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 10:25:52 by cado-car          #+#    #+#             */
+/*   Updated: 2021/10/05 16:35:06 by cado-car         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fdf.h"
+
+static int	expose_handle(t_fdf *fdf);
+
+int	main(int argc, char **argv)
+{
+	t_fdf	*fdf;
+
+	if (argc != 2)
+		error(1);
+	init_fdf(&fdf);
+	render(fdf);
+	mlx_key_hook(fdf->win, &key_handle, fdf);
+	mlx_expose_hook(fdf->win, &expose_handle, fdf);
+	mlx_loop(fdf->mlx);
+}
+
+static int	expose_handle(t_fdf *fdf)
+{
+	render(fdf);
+	return (0);
+}
